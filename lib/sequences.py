908626a45dd725e12411b85aea6927babb1b8f14
Mgridgelabs <mainaianndungu@gmail.com>
2024-08-31 20:14:13 +0300

    completes sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])  
+        return
+    elif length == 1:
+        print([0])  
+        return
+    fibonacci_sequence = [0, 1]
+    for i in range(2, length):
+        next_value = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2]
+        fibonacci_sequence.append(next_value)
+    print(fibonacci_sequence[:length])
+print_fibonacci(9)
